1a866507cf145637038d4f5ab92caaf9552a9620
Wasay-Rana <wasayrana2@gmail.com>
2024-02-14 15:37:27 +0500

    Profile Change :(

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import wasayImage from "public/wasay_ai.png"
+import wasayImage from "public/wasay.png"
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
