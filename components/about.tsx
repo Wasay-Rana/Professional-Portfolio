cd776bb5aa34afdbe740a4a6b71862f307e745e0
Wasay-Rana <wasayrana2@gmail.com>
2024-02-14 14:13:43 +0500

    New Updates, I Hope it is final

--- components/about.tsx
+++ components/about.tsx
@@ -22,18 +22,18 @@ export default function About() {
         Ever since I can remember, I've been hooked on two things: Predictive Modeling and Ethical Hacking. So, while I was busy securing my degree in{" "}
         <span className="font-medium">Computer Science</span>, I thought, why not dive into the world of Machine Learning? It's like the Swiss Army knife of tech - super versatile. My journey kicked off with Andrew Ng's Specialization, where I got cozy with{" "}
         <span className="font-medium">all the import stuff related to Pre-Processing, ML and DL modeling</span>.
-        You know what I love most about coding? Solving problems. There's this rush you get when you finally crack the code, and it's addicting! I mean, who doesn't dig that feeling?
+        You know what I love most is the ability to mimic human-like learning and decision-making processes to solve problems. There's this rush you get when you finally crack the code, and it's addicting! I mean, who doesn't dig that feeling?
         As for my go-to tools, you'll usually find me hanging out with{" "}
         <span className="font-medium">
           Python, Scikit-Learn, OpenCV, and Tensorflow
         </span>
-        , like a tech-savvy (Which I really am). And let's not forget about my pytorch skills and GAN adventures - they're like the secret sauce in my tech recipe.
-        But apart from this, I'm not one to sit still. Nope, I'm always up for learning something new (hence my ever-growing list of interests). Right now, I'm on the lookout for a{" "}
-        <span className="font-medium">remote ML Engineer gig</span>.
+        , like a tech-savvy (Which I really am 💻). In my tech toolkit, GANs and ANNs are like the Stark Industries' arc reactor - indispensable powerhouses. 
+        Beyond that, I'm always hungry for new challenges. Currently, seeking a {" "}
+        <span className="font-medium">remote ML Engineer</span> role to continue my journey.
       </p>
 
       <p>
-        <span className="italic">When I'm not in code mode,</span> you'll find me kicking back with a game of chess, wandering in the Open-World video games, or maybe even messing with my home network. Because who needs downtime when you can have uptime, right?
+        <span className="italic">When I'm not in code mode,</span> you'll find me kicking back with a game of chess, wandering in the Open-World video games (Especially Genshin), or maybe even messing with my home network. Because who needs downtime when you can have uptime, right?
       </p>
     </motion.section>
   );
